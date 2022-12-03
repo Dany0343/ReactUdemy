3b384a95e2f1f4bd088b966a0cb00b3e8bdd4f2c
Oscar Bucio <oscarbucio2001@gmail.com>
2022-12-03 13:17:03 -0600

    add: HelloWorld.jsx

---
+++ 03-counter-app-vite/src/HelloWorld.jsx
@@ -0,0 +1,6 @@
+// Usualmente todo en React comienza con un componente o un functional component, no son más que una función, son basados en funciones, también se trabaja con clases pero ya no son aconsejadas de trabajar en React, son compatibles pero se acostumbra trabajar con functional components
+
+function App() { // Todos los nombres de los functional components van con Pascal Case o Upper Camel Case, normalmente este componente no está aquí porque el hot reload de Vite lo está actualizando todo el tiempo
+
+    return <h1>Hola mundo!</h1>;
+}
