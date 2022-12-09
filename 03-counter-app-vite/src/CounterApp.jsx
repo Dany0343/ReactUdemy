2ffac1b1403d18b58ed627c2504a3e3572f72a98
Oscar Bucio <oscarbucio2001@gmail.com>
2022-12-09 02:31:21 -0600

    done: section 4

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,25 +1,43 @@
-import PropTypes from 'prop-types';
+// Primer Hook
+import { useState } from 'react'; // 'use' significa en pocas palabras que es un hook, es una regla
 
-// Se recomienda que si una función no ocupa nada del componente donde está alojada, se recomienda que esté fuera
-const handleAdd = ( event ) => { 
-    console.log(event) 
-}
+import PropTypes from 'prop-types';
 
 
 // Componente, functional component
 export const CounterApp = ({ value }) => {
 
+    const [ counter, setCounter ] = useState( value ) // Usualmente se requiere mandar cual es el valor inicial, es el valor del estado, se pone en const para evitar accidentalmente mutar el valor
+    // El valor se cambia con el segundo argumento
+
+    const handleAdd = () => { 
+        // console.log(event)
+        //setCounter( counter + 1 ); // No se pone ++ porque se estaría intentando acumular y eso daría un error
+        // Cuando se manda a llamar un setCounter lo que hace es decirle a React que el estado cambió y por consecuencia tiene que volver a ejecutar la renderización del componente, React es muy eficiente, solo el código que necesita ser cambiado cambia
+
+        // Otra forma
+       setCounter((c) => c + 1) // Se le manda un callback
+    }
+
+    const handleSubstract = () => {
+        setCounter( counter - 1 )
+    }
+
+    const handleReset = () => {
+        setCounter( value )
+        // El reset se va a cambiar directamente al valor que tiene value, en este caso 20 y value siempre sigue siendo 20, y no se debería cambiar la property como tal.
+    }
 
     return (
 
         // Fragment
         <>
             <h1>CounterApp</h1>
-            <p>{value}</p>
+            <p>{ counter }</p>
 
-            <button onClick={ handleAdd }>
-                +1
-            </button>
+            <button onClick={ handleAdd }> +1 </button>
+            <button onClick={ handleSubstract }> -1 </button>
+            <button onClick={ handleReset }> Reset </button>
         </>
     )
 }
