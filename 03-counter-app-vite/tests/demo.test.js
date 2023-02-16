4316552aff0f4e38e71eecff12d79e71bddc6192
Oscar Bucio <oscarbucio2001@gmail.com>
2023-02-16 15:26:51 -0600

    add: tests

---
+++ 03-counter-app-vite/tests/demo.test.js
@@ -0,0 +1,13 @@
+
+
+test('Esta prueba no debe de fallar', () => {
+    // Pasos de una prueba (AAA)
+    // 1. Inicialización
+    const message1 = "Hola mundo";
+
+    // 2. Estímulo
+    const message2 = message1.trim();
+
+    // 3. Aserciones u Observar el comportamiento esperado
+    expect(message1).toBe(message2);
+});
