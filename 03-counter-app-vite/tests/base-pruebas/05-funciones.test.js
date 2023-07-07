0272dff3dd81d8bc7546a07e2d6f69ad8ffd06b8
Oscar Bucio <oscarbucio2001@gmail.com>
2023-07-06 18:08:18 -0600

    we back

---
+++ 03-counter-app-vite/tests/base-pruebas/05-funciones.test.js
@@ -0,0 +1,6 @@
+
+describe('Pruebas en 05-funciones', () => { 
+    test('getUser debe de retornar un objeto', () => {
+
+    })
+});
--- 03-counter-app-vite/tests/demo.test.js
+++ 03-counter-app-vite/tests/demo.test.js
@@ -1,13 +1,14 @@
 
-
-test('Esta prueba no debe de fallar', () => {
-    // Pasos de una prueba (AAA)
-    // 1. Inicialización
-    const message1 = "Hola mundo";
-
-    // 2. Estímulo
-    const message2 = message1.trim();
-
-    // 3. Aserciones u Observar el comportamiento esperado
-    expect(message1).toBe(message2);
+describe('Pruebas en <DemoComponent />', () => { 
+    test('Esta prueba no debe de fallar', () => {
+        // Pasos de una prueba (AAA)
+        // 1. Inicialización
+        const message1 = "Hola mundo";
+    
+        // 2. Estímulo
+        const message2 = message1.trim();
+    
+        // 3. Aserciones u Observar el comportamiento esperado
+        expect(message1).toBe(message2)
+    });
 });
