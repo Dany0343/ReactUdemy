bf62b0f555d16fce3fdc289ff30f430ae92bc3a7
Oscar Bucio <oscarbucio2001@gmail.com>
2023-07-28 16:44:19 -0600

    rollin out

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -3,8 +3,14 @@
 
 export const GifExpertApp = () => {
   return (
-    <>
+    <>  
+        {/* Titulo */}
         <h1>GifExpertApp</h1>
+
+        {/* Input  */}
+
+        {/* Listado de Gif*/}
+            {/* Gif Item */}
     </>
   )
 }
